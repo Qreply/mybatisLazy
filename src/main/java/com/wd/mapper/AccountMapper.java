93877493908508419119945bab319f8009c8b4ad
Qreply <1041334031@qq.com>
2019-10-25 13:03:39 +0800

    Initial commit

---
+++ src/main/java/com/wd/mapper/AccountMapper.java
@@ -0,0 +1,24 @@
+package com.wd.mapper;
+
+import com.wd.entity.Account;
+
+import java.util.List;
+
+public interface AccountMapper {
+
+    /**
+     * 查询所有账户,同时还要获取到当前账户的所属用户信息
+     * @return
+     */
+    List<Account> findAll();
+
+    /**
+     * 根据用户id查询账户信息
+     * @param uid
+     * @return
+     */
+    List<Account> findAccountByUid(Integer uid);
+
+
+
+}
